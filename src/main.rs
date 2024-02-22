052fe71f4fea320022eab95888110a685ef57a92
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-22 15:08:12 +0100

    Moved implementation to bitcoin client module

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,9 @@
-use std::{
-    net::{IpAddr, Ipv4Addr, SocketAddr},
-    str::FromStr,
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
-
-use bitcoin::{
-    consensus::{deserialize_partial, serialize},
-    network::Network,
-    p2p::{
-        address,
-        message::{NetworkMessage, RawNetworkMessage},
-        message_network::VersionMessage,
-        ServiceFlags,
-    },
-};
-
-use bytes::BytesMut;
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use p2p_handshake_bitcoin::bitcoint_client::BitcoinClient;
 
 #[tokio::main]
 async fn main() {
-    let dest_socket = "45.9.148.241:8333";
-    let user_agent = "/Satoshi:26.0.0/";
-    let timeout: u64 = 200;
-
-    let now = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs() as i64;
-
-    let no_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
-    let node_socket = SocketAddr::from_str(dest_socket).unwrap();
-
-    let dest_address = address::Address::new(&node_socket, ServiceFlags::NONE);
-    let source_address = address::Address::new(&no_address, ServiceFlags::NONE);
-
-    let btc_version = VersionMessage::new(
-        ServiceFlags::NONE,
-        now,
-        dest_address,
-        source_address,
-        now as u64,
-        user_agent.to_string(),
-        0,
-    );
-
-    let raw_message = RawNetworkMessage::new(
-        Network::Bitcoin.magic(),
-        NetworkMessage::Version(btc_version),
-    );
-
-    let stream = tokio::time::timeout(
-        Duration::from_millis(timeout),
-        TcpStream::connect(dest_socket),
-    )
-    .await
-    .unwrap()
-    .unwrap();
-
-    let (rx_stream, mut tx_stream) = stream.into_split();
-
-    let data = serialize(&raw_message);
-    tx_stream.write_all(data.as_slice()).await.unwrap();
-
-    let mut read_buffer = BytesMut::with_capacity(512);
-    loop {
-        if let Ok((message, count)) = deserialize_partial::<RawNetworkMessage>(&read_buffer) {
-            println!("{}, {}", message.cmd(), count);
-            break;
-        }
-        match rx_stream.try_read_buf(&mut read_buffer) {
-            Ok(value) => {
-                if value == 0 {
-                    if read_buffer.is_empty() {
-                        println!("Empty buffer");
-                    } else {
-                        println!("Connection error");
-                    }
-                }
-            }
-            Err(_) => {
-                continue;
-            }
-        }
-    }
+    let dest_uri = "45.9.148.241:8333";
+    let timeout = 200;
+    let bitcoin_client = BitcoinClient::new(dest_uri.to_string(), timeout);
+    bitcoin_client.handshake().await;
 }
