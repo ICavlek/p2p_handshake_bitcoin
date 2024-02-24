4a0d53e87ac319aa19737608c110f98973914f57
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-24 13:34:15 +0100

    Updated test, first happy test successfully integrated - Test SUCCESS

--- tests/p2p_handshake_bitcoin/bitcoin_client.rs
+++ tests/p2p_handshake_bitcoin/bitcoin_client.rs
@@ -5,16 +5,17 @@ use crate::helper::BitcoinNodeMock;
 #[tokio::test]
 async fn bitcoin_node_responds_with_version_and_verack_message() {
     let bitcoin_mock_node = BitcoinNodeMock::default();
-    let bitcoin_client = BitcoinClient::new(bitcoin_mock_node.reader, bitcoin_mock_node.writer);
+    let mut bitcoin_client = BitcoinClient::new(bitcoin_mock_node.reader, bitcoin_mock_node.writer)
+        .expect("Failed to create Bitcoin client");
+
     let bitcoin_version_message = BitcoinMessage::version_message();
-    match bitcoin_client
-        .expect("Failed to create client")
+    bitcoin_client
         .handle_message(bitcoin_version_message)
         .await
-    {
-        Ok((message, count)) => dbg!(message, count),
-        Err(_) => dbg!(BitcoinMessage::version_message(), 99),
-    };
-
-    assert!(true);
+        .expect("Failed to exchange version messages");
+    let bitcoin_verack_message = BitcoinMessage::verack_message();
+    bitcoin_client
+        .handle_message(bitcoin_verack_message)
+        .await
+        .expect("Failed to exchange verack messages");
 }
--- tests/p2p_handshake_bitcoin/helper.rs
+++ tests/p2p_handshake_bitcoin/helper.rs
@@ -13,10 +13,12 @@ impl Default for BitcoinNodeMock {
         let bitcoin_verack_message = BitcoinMessage::verack_message();
         Self {
             reader: tokio_test::io::Builder::new()
+                .read(serialize(&bitcoin_version_message).as_slice())
                 .read(serialize(&bitcoin_verack_message).as_slice())
                 .build(),
             writer: tokio_test::io::Builder::new()
                 .write(serialize(&bitcoin_version_message).as_slice())
+                .write(serialize(&bitcoin_verack_message).as_slice())
                 .build(),
         }
     }
