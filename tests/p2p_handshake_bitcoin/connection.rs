c9614f6283fe1b33078df2f52dc8c4709360d34f
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-24 21:27:54 +0100

    Test for bad slice response on Connection

--- tests/p2p_handshake_bitcoin/connection.rs
+++ tests/p2p_handshake_bitcoin/connection.rs
@@ -32,3 +32,16 @@ async fn bitcoin_node_responds_with_version_and_verack_message() {
     assert_eq!(message, BitcoinMessage::verack_message());
     assert_eq!(count, 24);
 }
+
+#[tokio::test]
+async fn bitcoin_node_responds_with_bad_u8_slice() {
+    let bitcoin_mock_node = BitcoinNodeMock::bad_u8_slice_response_on_version_message();
+    let mut connection = Connection::new(bitcoin_mock_node.reader, bitcoin_mock_node.writer);
+
+    let bitcoin_version_message = BitcoinMessage::version_message();
+    let _ = connection
+        .write(serialize(&bitcoin_version_message).as_slice())
+        .await;
+    let response = connection.read::<RawNetworkMessage>().await;
+    assert!(response.is_err());
+}
