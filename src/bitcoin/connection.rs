34a87b63c28f09636d157898a2671dc59afd69a6
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-27 14:47:18 +0100

    Docu for connection done

--- src/bitcoin/connection.rs
+++ src/bitcoin/connection.rs
@@ -2,6 +2,7 @@ use bitcoin::consensus::{deserialize_partial, Decodable};
 use bytes::{Buf, BytesMut};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
+/// Module that handles connection and message exchange with Bitcoin node
 pub struct Connection<Reader, Writer>
 where
     Reader: AsyncReadExt + Unpin,
@@ -17,6 +18,8 @@ where
     Reader: AsyncReadExt + Unpin,
     Writer: AsyncWriteExt + Unpin,
 {
+    /// Creates a module that contains reading stream, writing stream and
+    /// a buffer for incoming messages.
     pub fn new(rx_stream: Reader, tx_stream: Writer) -> Connection<Reader, Writer>
     where
         Reader: AsyncReadExt + Unpin,
@@ -29,6 +32,8 @@ where
         }
     }
 
+    /// Reads an incoming message and deserializes it into a struct that implements
+    /// trait Decodable.
     pub async fn read<T: Decodable>(&mut self) -> Result<Option<(T, usize)>, anyhow::Error> {
         loop {
             if let Ok((message, count)) = deserialize_partial::<T>(&self.buffer) {
@@ -46,6 +51,7 @@ where
         }
     }
 
+    /// Writes a chunk of u8's to a writing stream.
     pub async fn write(&mut self, message: &[u8]) -> Result<(), anyhow::Error> {
         self.tx_stream.write_all(message).await?;
         Ok(())
--- src/bitcoin/mod.rs
+++ src/bitcoin/mod.rs
@@ -1,7 +1,8 @@
-/// Client that is used to establish communication with the remote node.
+/// Client that is used to establish communication with the remote node
 pub mod client;
 /// Module to handle multiple bitcoin client handshakes
 pub mod client_pool;
+/// Module that handles connection and message exchange with Bitcoin node
 pub mod connection;
 pub mod message;
 pub mod stream;
